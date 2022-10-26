61088c642f2c099247de4d30212f562b08dc90b7
rhit-caiy <caiy@rose-hulman.edu>
2022-10-26 12:31:23 -0400

    Update databaseproject.py
    add sign up, log in, login page

--- UI/databaseproject.py
+++ UI/databaseproject.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 #pip install pymssql in console
 import pymssql
-from tkinter import Canvas,Tk,messagebox
+from tkinter import Canvas,Tk,messagebox,simpledialog
 import hashlib
 
 window=Tk()
-canvas=Canvas(window,bg="#DDDDDD",width=1400,height=660)#canvas size
+canvas=Canvas(window,bg="#EEEEEE",width=1400,height=660)#canvas size
 window.title("meal plan")
 
 #server, user, password, database
@@ -16,31 +16,36 @@ database="10_MealPlan"
 
 islogin=False
 username=""
+name=""
 savepassword=False
 userpassword=""
 
 spname=["insert_Person","delete_person","insert_Food","insert_Ingredient","update_name","insert_IsIngredientOf","insert_mealplan","add_mealpan_to_person","delete_mealplan"]
 spargs={}
 
-spviewname=[]
+spviewname=["get_Food","get_Ingredient"]
 spviewargs={}
 
 spdisplayname=[]#name of sp that access data in database
 page=-1
-#0: start page, require login
-#1: main page after log in
-#2... : pages to display informations and modify
-#
+
+#current plan on page
+#0: start page, require login or sign up
+#1: main page after log in, able to link to many pages below as well as log out
+#2: personal information
+#3: display all food, able to add, connect food to ingredient
+#4: display all ingredient, able to add
+#5: show all personal meal plan, add or edit meal plan
+#6: 
 #-1: display most table version
 
-#show content of one table on UI. 
+#show content of one table on UI, can only be used for test purpose
 def display(databasetablename,attribute,column):
     r=0#row number used for text coordinate
     canvas.create_text(120*column+40,80,text=databasetablename)
     canvas.create_text(120*column+40,100,text=attribute)
     with pymssql.connect(server,user,password,database) as conn:
         cursor = conn.cursor(as_dict=True)
-        #TODO: add stored procedure and display content from its return
         cursor.execute('SELECT * FROM '+databasetablename)
         for row in cursor:
             r+=1
@@ -94,20 +99,55 @@ def draw():
         
         canvas.create_rectangle(910,500,990,550)
         canvas.create_text(950,525,text="delete meal plan")
+        
+        canvas.create_rectangle(1000,500,1100,550)
+        canvas.create_text(1050,525,text="go to main page")
+    elif page==0:
+        canvas.create_text(700,300,text="Meal Plan",font=("Purisa",30))
+        
+        canvas.create_rectangle(650,400,750,450,fill="#AAAAAA")
+        canvas.create_text(700,425,text="log in")
+        
+        canvas.create_rectangle(650,500,750,550,fill="#AAAAAA")
+        canvas.create_text(700,525,text="sign up")
+    elif page==1:
+        canvas.create_text(700,300,text="Log in now",font=("Purisa",30))
+        
 
 #detect click event on canvas, x and y are integer from up left to bottom right on canvas
 def click(coordinate):
+    global page
     x=coordinate.x
     y=coordinate.y
     #detect coordinate on canvas and choose event
+    
+    if page==0:
+        if 650<x<750 and 400<y<450:
+            #log in
+            username=simpledialog.askstring(title="username", prompt="input your username", initialvalue="")
+            password=simpledialog.askstring(title="password", prompt="input your password", initialvalue="")
+            password=hashlib.sha256(password.encode('utf-8')).hexdigest()
+            r=callsp("exist_Person",(username,password))
+            if r:
+                print("successfully log in")
+                page=1
+            else:
+                messagebox.showerror('error',"not exist user or incorrect password")
+        if 650<x<750 and 500<y<550:
+            username=simpledialog.askstring(title="username", prompt="input your username", initialvalue="")
+            user_name=simpledialog.askstring(title="password", prompt="input your name", initialvalue="")
+            password=simpledialog.askstring(title="password", prompt="input your password", initialvalue="")
+            password=hashlib.sha256(password.encode('utf-8')).hexdigest()
+            if callsp("insert_Person",(username,user_name,password)):
+                messagebox.showinfo("successfully sign up","Please log in use account")
     if page==-1:
         if 100<x<200 and 500<y<550:
             print("insert person")
+            personUsername=input("person Username:")
             personName=input("person name:")
-            personUsername=input("person username:")
             personPassword=input("password:")
             personPassword=hashlib.sha256(personPassword.encode('utf-8')).hexdigest()
-            callsp("insert_Person",(personName,personUsername,personPassword))
+            callsp("insert_Person",(personUsername,personName,personPassword))
             
         if 200<x<300 and 500<y<550:
             print("delete person")
@@ -175,35 +215,50 @@ def click(coordinate):
             t=input("type:")
             callsp("delete_mealplan", (mealplanID,date,t))
         
+        if 1000<x<1100 and 500<y<550:
+            page=0
         
     canvas.delete("all")
     draw()
     
 #stored procedure, show a dialog if error
 def callsp(spname,args):
+    print(spname,args)
     try:
         with pymssql.connect(server,user,password,database) as conn:
-            cursor = conn.cursor(as_dict=True)
-            cursor.callproc(spname, args)
-            conn.commit()
-            print("finish stored procedure")
+            with conn.cursor(as_dict=True) as cursor:
+                #cursor = conn.cursor(as_dict=True)
+                cursor.callproc(spname, args)
+                r=[]
+                for row in cursor:
+                    r.append(row)
+                    print(row)
+                conn.commit()
+                print("finish stored procedure")
+                # cnt=cursor.fetchall()
+                print(cursor)
+                
+                if r:
+                    return r
+                else:
+                    return True
     except pymssql._pymssql.DatabaseError as e:
         print("DatabaseError on stored procedure \"",spname,"\" with args",args)
         print(e)
-        error=str(e).split("b\'")[1]
+        error=(str(e)+"b\'DB-Lib").split("b\'")[1]
         error=error.split("DB-Lib")[0]
         messagebox.showerror('error',error)
     except pymssql._mssql.MSSQLDatabaseException as e:
         print("MSSQLDatabaseException on stored procedure \"",spname,"\" with args",args)
-        error=str(e).split("b\'")[1]
+        error=(str(e)+"b\'DB-Lib").split("b\'")[1]
         error=error.split("DB-Lib")[0]
         messagebox.showerror('error',error)
     except:
         print("Unknown error on stored procedure \"",spname,"\" with args",args)
         messagebox.showerror('error',"error")
+    return False
         
-#TODO: add more stored procedure
-#add variables to control page
+#TODO: add more pages and their associated stored procedure
 canvas.bind("<Button-1>",click)
 
 draw()
